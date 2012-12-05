5a87a8f15f062ec7013d3e718ec4d327513e25bb
Frank Roland <meet_thrall@freenet.de>
2012-12-05 12:27:28 +0100

    Added cyclic loop

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ keep_web_alive
 ==============
 
 Simulate user activity in order to keep IP connection alive.
+
+
+Configuration
+-------------
+
+config.yaml:
+> ---
+> :proxy: http://proxy-rob.roboter.kuka.de
+> :search: http://www.google.com
+> :ip: ifconfig.me/ip
+> :silent: false
+> :cyclic: false
+> :sleep: 10
--- keep_web_alive.rb
+++ keep_web_alive.rb
@@ -7,8 +7,19 @@ config_file = File.join(base_path, 'config.yml')
 user_config = File.exists?(config_file) ? YAML.load_file(config_file) : {}
 user_config[:search] = "http://www.google.com" unless user_config[:search]
 user_config[:ip] = "ifconfig.me/ip"            unless user_config[:ip]
+user_config[:silent] = false                   unless user_config[:silent]
+user_config[:cyclic] = false                   unless user_config[:cyclic]
+user_config[:sleep] = 10                       unless user_config[:sleep]
 
 RestClient.proxy = user_config[:proxy] if user_config[:proxy]
 
-RestClient.get(user_config[:search], q: SecureRandom.urlsafe_base64(20)) if user_config[:search]
-puts RestClient.get(user_config[:ip])                                    if user_config[:ip]
+loop do
+   RestClient.get(user_config[:search], q: SecureRandom.urlsafe_base64(20)) if user_config[:search]
+   if user_config[:ip]
+      ip = RestClient.get(user_config[:ip])
+      puts ip unless user_config[:silent]
+   end
+   sleep(user_config[:sleep])
+   break unless user_config[:cyclic]
+end
+
